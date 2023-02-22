8d91588d3ef80d3e8e887ae4ffc05c307710a49c
tina458615 <69339812+tina458615@users.noreply.github.com>
2023-02-22 15:53:09 +0800

    更新

--- TCA/js/getContentR7_3.js
+++
@@ -1,3 +0,0 @@
-var input_tsai_poe_hoe = document.getElementById("getContentR7_3");
-
-if(input_tsai_poe_hoe) var tsai_poe_hoe = input_tsai_poe_hoe.value.replace(/(.{8})/g, "$1\n");
--- TCA/js/printTablePDF.js
+++ TCA/js/printTablePDF.js
@@ -1,15 +1,11 @@
-var downloadPDF = document.getElementById("downloadPDF");
-downloadPDF.addEventListener('click', downloadAsPDF);
-
-
-/*
-var contentR1 = '';
-var contentR2 = '';
 var contentR3 = '';
+var contentR1 = '';
 var contentR4 = '';
-var timeRecord = '';
-var finalDecision = '';
-*/
+var contentR2 = '';
+var taiwanEmpire = '';
+var peh_oe_ji = '';
+var tsai_poe_hoe = '';
+var recorderName = '';
 
 function getDate() {
     var today = new Date();
@@ -21,22 +17,12 @@ function getDate() {
 }
 
 function downloadAsPDF(){
-    var doc = new jsPDF('p', 'pt', 'a4');
-    //var actionTable = canvas.toDataURL("img/actionTable.png");
-	/*
-	contentR1 = document.getElementById('reason_one_input').value.replace(/(.{9})/g, "$1\n");
-    contentR2 = document.getElementById('reason_two_input').value.replace(/(.{9})/g, "$1\n");
-    contentR3 = document.getElementById('reason_three_input').value.replace(/(.{9})/g, "$1\n");
-    contentR4 = document.getElementById('reason_four_input').value.replace(/(.{9})/g, "$1\n");
-    timeRecord = document.getElementById('timeRecord').value.replace(/(.{9})/g, "$1\n");
-    finalDecision = document.getElementById('finalDecision').value.replace(/(.{9})/g, "$1\n");
-	*/
+    var doc = new jsPDF('p', 'pt', 'a4');   
 	doc.addFileToVFS('iansuiRegular.ttf', font);
 	doc.addFont('iansuiRegular.ttf', 'iansuiRegular', 'normal');
 	doc.setFont('iansuiRegular');
     html2canvas(document.body, {
     onrendered: function(canvas) {
-        //<img src="img/actionTable.png" id="actionTable">
 		doc.addImage(actionTable, 'JPEG', 0, 0, 595, 840);
         doc.setTextColor(117,28,28);
         doc.text(contentR3, 40, 240);
